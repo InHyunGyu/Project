b5a267b71d4da54e18e4099b33ab48101147324c
InHyunGyu <inhyungyu1@gmail.com>
2019-09-05 18:12:44 +0900

    190905 0612 MemberController

--- src/main/java/com/debugking/www/MemberController.java
+++ src/main/java/com/debugking/www/MemberController.java
@@ -2,6 +2,7 @@ package com.debugking.www;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,7 +14,7 @@ public class MemberController {
 	MemberRepository repo;
 	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String login() {
+	public String login(Model model) {
 		
 		
 		return "index";
